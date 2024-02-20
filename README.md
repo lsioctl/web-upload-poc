ceea25b242df6481545f2c487697567f22bd21da
lsioctl <lsioctl@gmail.com>
2024-02-20 18:50:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## web-upload
+
+Small PoC for file upload with Nest backend
+
+## Links
+
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file
+https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
+https://docs.nestjs.com/techniques/file-upload
+
+## frontend
+
+### html files
+
+A simple static webserver is enough, e.g:
+
+```bash
+python3 -m http.server 8000
+```
+
+## Backend
+
+Nest classical launch
+
---
+++ nest-backend/fileupload-server/src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file2'))
+  uploadFile(@UploadedFile() file2: Express.Multer.File) {
+    console.log(file2);
+  }
+
+}
